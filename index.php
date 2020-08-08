063658b44faa4d9b86703a70b809e0fe47699fcb
braddevans <braddevans123@gmail.com>
2020-08-08 15:15:58 +0100

    cleanup index and remove a.php

--- index.php
+++ index.php
@@ -6,18 +6,39 @@
 </head>
 
 <body>
-<div class="card border-primary mb-3" style="max-width: 38rem;">
-    <div class="card-body">
-        <h5 class="card-title">CodersRank</h5>
-        <codersrank-widget username="braddevans"></codersrank-widget>
-        <a href="https://profile.codersrank.io/user/braddevans" class="btn btn-primary">To CodersRank Profile</a>
-    </div>
-</div>
-<div class="card border-primary mb-3" style="max-width: 38rem;">
-    <div class="card-body">
-        <h5 class="card-title">Code::Stats</h5>
-        <p>Day to Day Stats of programming</p>
-        <a href="https://codestats.net/users/slicedbread" class="btn btn-primary">To Code::Stats Profile</a>
+<br />
+<div class="container" style="width: 100%">
+    <div class="row" style="width: 100%">
+        <div class="col-sm" style="width: 25%;">
+            <div class="card border-primary mb-3" style="max-width: 38rem;">
+                <div class="card-body">
+                    <h5 class="card-title">CodersRank</h5>
+                    <codersrank-widget username="braddevans"></codersrank-widget>
+                    <a href="https://profile.codersrank.io/user/braddevans" class="btn btn-primary">To CodersRank Profile</a>
+                </div>
+            </div>
+            <div class="card border-primary mb-3" style="max-width: 38rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Code::Stats</h5>
+                    <p>Day to Day Stats of programming</p>
+                    <a href="https://codestats.net/users/slicedbread" class="btn btn-primary">To Code::Stats Profile</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm" style="width: 50%;">
+            <div class="github-widget" data-username="braddevans"></div>
+        </div>
+        <div class="col-sm" style="width: 25%;">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    Planned Projects/ Projects being made
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">1, CRM Website for tickets and Issues</li>
+                    <li class="list-group-item">2, Game Remaster for a game made 13 years ago</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
 </body>
